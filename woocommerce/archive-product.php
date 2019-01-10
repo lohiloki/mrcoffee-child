6d3e5808c4c87b8bccef07c432111d1e31173563
Lohith M <lohitm86@gmail.com>
2019-01-10 17:16:28 +0530

    customize woocommerce archive page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -18,7 +18,24 @@
 defined( 'ABSPATH' ) || exit;
 
 get_header( 'shop' );
+?>
 
+<header class="woocommerce-products-header">
+	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+	<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+<?php endif; ?>
+
+	<?php
+	/**
+     * Hook: woocommerce_archive_description.
+     *
+     * @hooked woocommerce_taxonomy_archive_description - 10
+     * @hooked woocommerce_product_archive_description - 10
+     */
+	do_action( 'woocommerce_archive_description' );
+	?>
+	</header>
+<?php
 /**
  * Hook: woocommerce_before_main_content.
  *
@@ -30,21 +47,7 @@ do_action( 'woocommerce_before_main_content' );
 
 ?>
 
-<header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
 
-	<?php
-	/**
-	 * Hook: woocommerce_archive_description.
-	 *
-	 * @hooked woocommerce_taxonomy_archive_description - 10
-	 * @hooked woocommerce_product_archive_description - 10
-	 */
-	do_action( 'woocommerce_archive_description' );
-	?>
-</header>
 <?php
 if ( woocommerce_product_loop() ) {
 
