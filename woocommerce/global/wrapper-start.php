1d8bc97873401887174779d9c8d36972731f43ce
Lohith M <lohitm86@gmail.com>
2019-01-10 16:48:34 +0530

    update woocommerce gloabal head

--- woocommerce/global/wrapper-start.php
+++ woocommerce/global/wrapper-start.php
@@ -45,6 +45,6 @@ switch ( $template ) {
 		echo '<div id="primary" class="content-area twentysixteen"><main id="main" class="site-main" role="main">';
 		break;
 	default :
-		echo '<div id="primary" class="content-area"><main id="main" class="site-main" role="main">';
+		echo '<div id="primary" class="content-area row"><main id="main" class="site-main col-md-8" role="main">';
 		break;
 }
