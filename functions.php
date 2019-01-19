740346cccc9be9a2c12c7b3263c3769713b351ea
Lohith M <lohitm86@gmail.com>
2019-01-19 21:53:57 +0530

    update out of stock

--- functions.php
+++ functions.php
@@ -29,3 +29,17 @@ if (!function_exists('loop_columns')) {
     }
 }
 */
+
+add_filter( 'woocommerce_get_availability', 'wcs_custom_get_availability', 1, 2);
+function wcs_custom_get_availability( $availability, $_product ) {
+
+    // Change In Stock Text
+    if ( $_product->is_in_stock() ) {
+        $availability['availability'] = __('Available!', 'woocommerce');
+    }
+    // Change Out of Stock Text
+    if ( ! $_product->is_in_stock() ) {
+        $availability['availability'] = __('Out of Stock - Available on Amazon from 21 Jan 2019', 'woocommerce');
+    }
+    return $availability;
+}
