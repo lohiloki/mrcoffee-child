be6d7ee72a0050b177a7086ed4bb3b66cde30ef9
LohithM <lohitm86@gmail.com>
2019-02-05 20:59:13 +0530

    update quantity nav now

---
+++ full-width-page.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LohithM
+ * Date: 05-02-2019
+ * Time: 20:54
+ */
+
+/*
+*Template Name: Full width Page
+*/
+/**
+ * The template for displaying pages
+ *
+ * @package    WordPress
+ * @subpackage Mrcoffee
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+	<?php
+	/** Display Slider*/
+	get_template_part( 'template-parts/blocks/block', 'slider' ); ?>
+
+<?php if ( have_posts() ) :
+    // Start the loop.
+    while ( have_posts() ) : the_post();
+        /** Display Header for Page*/
+        get_template_part( 'template-parts/header/header', 'head' ); ?>
+
+        <!-- PAGE CONTENT -->
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <!-- .entry-content -->
+            <div class="entry-content container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php the_title(); ?>
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            </div>
+            <!-- .entry-content end-->
+        </article>
+        <!-- PAGE CONTENT END -->
+    <?php
+        // End of the loop.
+    endwhile;
+
+endif;
+?>
+
+<?php get_footer(); ?>
